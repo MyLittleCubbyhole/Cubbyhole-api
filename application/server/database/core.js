8d69922ea9e8d1622a8ccdffbc1e0ea2a4c352b6
Julien Torrielli <julien.torrielli@gmail.com>
2014-09-10 23:17:31 +0200

    modifs config mongo

--- application/server/database/core.js
+++ application/server/database/core.js
@@ -20,18 +20,20 @@ database.init = function() {
 
 	if(!db.mongo) {
 		var	server = new Server(config['mongodb_auth'].host, config['mongodb_auth'].port, config['mongodb_auth']['server_options']);
-		db.mongo = new database(config['mongodb_auth'].database, server, config['mongodb_auth']['database_options']);
+		db.mongo = new database(config['mongodb_auth'].database.name, server, config['mongodb_auth'].database.options);
 
 		db.mongo.open(function(error, db) {
 			if(!error) {
-				console.log('mongodb connected')
-				db.collection('directories', function(error, collection){
-					collection.findOne({'_id': '1/admin' }, function(error, data) {
-						if(!data)
-							collection.insert({_id: '1/userPhotos', ownerId: 1, creatorId: 1, path: '/', name: 'userPhotos', type: 'folder', size: 0, lastUpdate: new Date(), undeletable: true, children: []}, { safe : true }, function() {
-									collection.insert({_id: '1/admin', ownerId: 1, creatorId: 1, path: '/', name: 'admin', type: 'folder', size: 0, lastUpdate: new Date(), undeletable: true, children: []}, { safe : true }, function() {
-									})
-							})
+				db.authenticate(config['mongodb_auth'].database.user, config['mongodb_auth'].database.password, function(error2, data2) {
+					console.log('mongodb connected')
+					db.collection('directories', function(error, collection){
+						collection.findOne({'_id': '1/admin' }, function(error, data) {
+							if(!data)
+								collection.insert({_id: '1/userPhotos', ownerId: 1, creatorId: 1, path: '/', name: 'userPhotos', type: 'folder', size: 0, lastUpdate: new Date(), undeletable: true, children: []}, { safe : true }, function() {
+										collection.insert({_id: '1/admin', ownerId: 1, creatorId: 1, path: '/', name: 'admin', type: 'folder', size: 0, lastUpdate: new Date(), undeletable: true, children: []}, { safe : true }, function() {
+										})
+								})
+						})
 					})
 				})
 			}
