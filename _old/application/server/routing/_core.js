f1f22d6b31b82f09baf1020af3c256f2b379f27f
Nicolas Gaignoux <gaignoux.nicolas@gmail.com>
2014-11-29 22:06:59 +0100

    adds auth controller + auth end point

---
+++ _old/application/server/routing/_core.js
@@ -0,0 +1,64 @@
+var file = require(global.paths.server + '/routing/rest/file')
+,	directory = require(global.paths.server + '/routing/rest/directory')
+,	user = require(global.paths.server + '/routing/rest/user')
+,	plan = require(global.paths.server + '/routing/rest/plan')
+,	multipartDecoder = require('connect-multiparty')()
+,	filters = require(global.paths.server + '/routing/filters/core')
+,	routing = {};
+
+routing.init = function(app) {
+
+	app.get('/api/activation', user.get.activateAccount);
+	app.get('/api/checkToken', filters.tokenInterceptor, user.get.checkToken);
+	app.get('/api/checkAdminToken', filters.tokenInterceptor, filters.adminInterceptor, user.get.checkToken);
+	app.get('/api/logout', user.get.logout);
+	app.post('/api/auth', user.post.authenticate);
+
+
+	app.get('/api/users', filters.tokenInterceptor, filters.adminInterceptor, user.get.all);
+	app.get(/^\/api\/users\/shared\/(\/?.+)+\/$/, filters.tokenInterceptor, user.get.usersBySharing);
+	app.get('/api/users/:email', filters.tokenInterceptor, user.get.byEmail);
+	app.get('/api/users/:id/plan', filters.tokenInterceptor, user.get.currentPlan);
+	app.get('/api/users/:id/quota', filters.tokenInterceptor, user.get.usedQuota);
+	app.get('/api/users/:id/historic', filters.tokenInterceptor, user.get.historic);
+	app.post('/api/users', multipartDecoder, user.post.create);
+	app.post('/api/users/:userId/plans/:planId', filters.tokenInterceptor, user.post.subscribe);
+    app.put('/api/users/:id', multipartDecoder, filters.tokenInterceptor, user.put.byId);
+	app.put('/api/users/:id/promote', filters.tokenInterceptor, filters.adminInterceptor, user.put.promote);
+	app.put('/api/users/:id/demote', filters.tokenInterceptor, filters.adminInterceptor, user.put.demote);
+
+	app.get('/api/plans', plan.get.all);
+	app.get('/api/plans/images', plan.get.images);
+
+	app.post('/api/plans', filters.tokenInterceptor, filters.adminInterceptor, plan.post.create);
+    app.put('/api/plans/:id', filters.tokenInterceptor, filters.adminInterceptor, plan.put.byId);
+	app.delete('/api/plans/:id', filters.tokenInterceptor, filters.adminInterceptor, plan.delete.byId);
+
+	app.get(/^\/api\/browse\/([0-9]+)$/, filters.tokenInterceptor, filters.rightInterceptor, directory.get.byOwner);
+	app.get(/^\/api\/browse\/([0-9]+)\/size$/, filters.tokenInterceptor, filters.rightInterceptor, directory.get.size);
+	app.get(/^\/api\/browse\/([0-9]+)\/(\/?.+)*/, filters.tokenInterceptor,filters.rightInterceptor, directory.get.byPath);
+    app.put(/^\/api\/browse\/([0-9]+)\/(\/?.+)+/, filters.tokenInterceptor, filters.rightInterceptor, directory.put.rename);
+	app.delete(/^\/api\/browse\/([0-9]+)$/, filters.tokenInterceptor, filters.rightInterceptor, directory.delete.byOwner);
+	app.delete(/^\/api\/browse\/([0-9]+)\/(\/*.+)+/, filters.tokenInterceptor, filters.rightInterceptor, directory.delete.byPath);
+	app.get(/^\/api\/download\/([0-9]+)\/$/, filters.tokenInterceptor, filters.rightInterceptor, file.get.zip);
+	app.get(/^\/api\/download\/([0-9]+)\/?(\/?.+)\/$/, filters.tokenInterceptor, filters.rightInterceptor, file.get.zip);
+	app.get(/^\/api\/download\/([0-9]+)\/(\/?.+)+/, filters.tokenInterceptor, filters.rightInterceptor, file.get.download);
+	app.get(/^\/api\/share\/([0-9]+)\/(\/?.+)+/, filters.tokenInterceptor, filters.rightInterceptor, file.get.share);
+	app.get(/^\/api\/unshare\/([0-9]+)\/(\/?.+)+/, filters.tokenInterceptor, filters.rightInterceptor, file.get.unshare);
+	app.get(/^\/api\/download\/shared\/(.+)+/, file.get.sharedPreview);
+	app.get(/^\/api\/shared\/(.+)+/, file.get.shared);
+	app.post(/^\/api\/download\/([0-9]+)\/$/, filters.tokenInterceptor, filters.rightInterceptor, file.post.zip);
+	app.post(/^\/api\/share\/([0-9]+)(\/?.+)*\/$/, filters.tokenInterceptor, filters.rightInterceptor, directory.post.share);
+	app.post(/^\/api\/unshare\/([0-9]+)(\/?.+)*\/$/, filters.tokenInterceptor, filters.rightInterceptor, directory.post.unshare);
+	app.post(/^\/api\/browse\/([0-9]+)(\/?.+)*\/$/, filters.tokenInterceptor, filters.rightInterceptor, directory.post.create);
+	app.post(/^\/api\/copy\/([0-9]+)(\/?.+)*$/, filters.tokenInterceptor, filters.rightInterceptor, directory.post.copy);
+	app.post(/^\/api\/move\/([0-9]+)(\/?.+)*$/, filters.tokenInterceptor, filters.rightInterceptor, directory.post.move);
+	app.post(/^\/api\/upload\/([0-9]+)(\/?.+)*\/$/, multipartDecoder, filters.tokenInterceptor, filters.rightInterceptor, filters.mobileInterceptor, file.post.upload);
+	
+	app.post('/api/paypalNotify', user.post.paypalNotify);
+
+}
+
+
+
+module.exports = routing;
---
+++ server/v0/auth/controllers/auth.es6
@@ -0,0 +1,35 @@
+/*Parent class cloning*/
+
+	var Controller = require('kanto-patterns').controller.clone();
+
+/*Services requiring*/
+
+	var AuthService = require(__dirname + '/../services/auth');
+
+/*Attributes definitions*/
+
+	Controller._name = 'Auth';
+
+/*Overridden methods declarations*/
+
+/*Private methods declarations*/
+
+/*Public methods declarations*/
+
+	Controller.post.auth = authenticate;
+
+module.exports = Controller;
+
+/*Overridden methods definitions*/
+
+/*Private methods definitions*/
+
+/*Public methods definitions*/
+
+	function authenticate(request, response, next) {
+		
+		Controller.isDefined({email: request.body.email, password: request.body.password})
+			.then((parameters) => AuthService.authenticate(parameters.email, parameters.password))
+			.then((token) => response.json({token: token}))
+			.catch((error) => next(error));
+	}
